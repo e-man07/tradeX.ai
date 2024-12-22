89768b8cef1c0a937494ddd0775049a68d61c895
shivendram16053 <shivendramishra.sm93@gmail.com>
2024-12-23 00:17:59 +0530

    check

--- app/(root)/agent/page.tsx
+++ app/(root)/agent/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import React, { useState } from "react";
-import Wallet from "@/component/Wallet";
+import Wallet from "@/components/Wallet";
 import {useWallet} from "@/hooks/useWallet";
-import Chat from "@/component/Chat";
+import Chat from "@/components/Chat";
+import { useSolanaAgent } from "@/hooks/useSolanaAgent";
 
 export default function Agent() {
   const {isAuthenticated}=useWallet();
