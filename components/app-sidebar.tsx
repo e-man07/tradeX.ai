9f0819be7be91cc153cbc557198d8aee2dcf6425
e-man07 <amann.jha1107@gmail.com>
2025-02-16 17:11:14 +0530

    minor changes

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -58,7 +58,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <TeamSwitcher teams={[{ name: "tradeX.ai" }]} />
+        <TeamSwitcher teams={[{ name: "tradeX" }]} />
       </SidebarHeader>
 
       <SidebarContent>
